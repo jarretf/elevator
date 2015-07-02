28d2388c68c6a4bfd5af83713a6a3ae89ef960d5
jarretf <jarret.java@gmail.com>
2015-07-02 17:09:42 +0530

    11) version-11 Final (Clean OO Architecture) (W/o DPs)

--- Elevator_1312_1340_1360/src/MainPrg.java
+++ Elevator_1312_1340_1360/src/MainPrg.java
@@ -7,20 +7,20 @@ public class MainPrg
 		Building bldg=new Building(6);
 		
 		//Elevator is at ground floor, you are at third floor, you want to go to ground floor
-		//bldg.press_floor_button(3, 0);
-		
+			bldg.elevator.current_floor.floor_no=0;
+			//bldg.press_floor_button(3, 0);
 		
 		//Elevator is at top floor, you are on third floor, you want to go to ground floor
-		//bldg.elevator.current_floor.floor_no=5;
-		//bldg.press_floor_button(3, 0);
+			bldg.elevator.current_floor.floor_no=5;
+			//bldg.press_floor_button(3, 0);
 		
 		//Elevator is at fifth floor(top most floor), you are at fifth floor, you want to go to second floor
-		//bldg.elevator.current_floor.floor_no=5;
-		//bldg.press_floor_button(5, 0);
+			bldg.elevator.current_floor.floor_no=5;
+			//bldg.press_floor_button(5, 0);
 		
 		//Elevator is at ground floor, you are at fifth floor, you want to go to ground floor
-		//bldg.elevator.current_floor.floor_no=0;
-		//bldg.press_floor_button(5, 0);
+			bldg.elevator.current_floor.floor_no=0;
+			//bldg.press_floor_button(5, 0);
 	}
 
 }
